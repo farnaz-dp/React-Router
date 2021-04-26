ea787cf80e1e7faface363eda1a9d2f4ecfb817e
farnaz <farnaz.dp7394@gmail.com>
2021-04-26 12:01:01 +0430

    App.js added to Git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react'
+import {Provider, useSelector} from "react-redux"
+import {BrowserRouter , Route ,Switch} from "react-router-dom"
+import {store} from "./Store"
+import {AuthRoute} from './Components/AuthRoute'
+import {Login} from "./Components/Login"
+import {Home} from "./Components/Home"
+import {About} from "./Components/About"
+// import {ProtectedRoute} from './Components/AuthRoute'
+import {PrivateRoute} from './Components/AuthRoute'
+import {AppRoute} from './Components/AppRoute'
+import {Header} from "./Components/Header";
+import {MainApp} from "./Components/MainApp";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+               <MainApp/>
+            </BrowserRouter>
+
+        </Provider>
+    );
 }
 
 export default App;
